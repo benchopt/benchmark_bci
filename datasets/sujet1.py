5d283c8b48407cc86411c8d00528eeae997aa0d9
chris-mrn <109912652+chris-mrn@users.noreply.github.com>
2023-06-02 16:20:02 +0200

    rename

--- datasets/sujet1.py
+++ datasets/sujet1.py
@@ -17,7 +17,7 @@ with safe_import_context() as import_ctx:
 class Dataset(BaseDataset):
 
     # Name to select the dataset in the CLI and to display the results.
-    name = "sujet1"
+    name = "BNCIsujet1"
 
     # List of parameters to generate the datasets. The benchmark will consider
     # the cross product for each key in the dictionary.
--- datasets/sujet2.py
+++ datasets/sujet2.py
@@ -18,7 +18,7 @@ with safe_import_context() as import_ctx:
 class Dataset(BaseDataset):
 
     # Name to select the dataset in the CLI and to display the results.
-    name = "sujet2"
+    name = "Zhousujet1"
 
     # List of parameters to generate the datasets. The benchmark will consider
     # the cross product for each key in the dictionary.
