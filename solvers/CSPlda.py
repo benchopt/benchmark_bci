b650085b3cad0047e97f88042d242e6610f462cc
chris-mrn <109912652+chris-mrn@users.noreply.github.com>
2023-06-06 09:53:45 +0200

    renamed

--- solvers/CSPlda.py
+++ solvers/CSPlda.py
@@ -29,8 +29,6 @@ class Solver(BaseSolver):
         self.X, self.y = X, y
         self.clf = make_pipeline(CSP(n_components=8), LDA())
 
-        # va chercher les meilleurs paramètres pour le modèle
-
     def run(self, n_iter):
         # This is the function that is called to evaluate the solver.
         self.clf.fit(self.X, self.y)
