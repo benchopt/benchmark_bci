5c441258114aba92806f8436e719f18c7fbbb273
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-07-03 17:07:40 +0200

    Changing the line limit

--- datasets/Simulated.py
+++ datasets/Simulated.py
@@ -1,16 +1,15 @@
 from benchopt import BaseDataset, safe_import_context
 
-
 # Protect the import with `safe_import_context()`. This allows:
 # - skipping import to speed up autocompletion in CLI.
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     from braindecode.datasets import MOABBDataset
+
     from benchmark_utils import windows_data
 
 
 class Dataset(BaseDataset):
-
     # Name to select the dataset in the CLI and to display the results.
     name = "simulated"
     # List of parameters to generate the datasets. The benchmark will consider
@@ -18,20 +17,20 @@ class Dataset(BaseDataset):
     # Any parameters 'param' defined here is available as `self.param`.
 
     def get_data(self):
-
         # The return arguments of this function are passed as keyword arguments
         # to `Objective.set_data`. This defines the benchmark's
         # API to pass data. It is customizable for each benchmark.
 
         dataset_name = "FakeDataset"
-        data = MOABBDataset(dataset_name=dataset_name,
-                            subject_ids=None,
-                            dataset_kwargs={"event_list": ["left_hand",
-                                                           "right_hand"],
-                                            "paradigm": "imagery"})
+        data = MOABBDataset(
+            dataset_name=dataset_name,
+            subject_ids=None,
+            dataset_kwargs={
+                "event_list": ["left_hand", "right_hand"],
+                "paradigm": "imagery",
+            },
+        )
 
-        dataset, sfreq = windows_data(data, 'LeftRightImagery')
+        dataset, sfreq = windows_data(data, "LeftRightImagery")
 
-        return dict(dataset=dataset,
-                    paradigm_name='LeftRightImagery',
-                    sfreq=sfreq)
+        return dict(dataset=dataset, paradigm_name="LeftRightImagery", sfreq=sfreq)
