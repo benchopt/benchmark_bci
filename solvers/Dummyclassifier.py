9cd7d14f409bb63c6721a4f551c92571f7d1beaf
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-07-04 17:16:19 +0200

    Changing logic for augmentation

--- solvers/Dummyclassifier.py
+++ solvers/Dummyclassifier.py
@@ -4,8 +4,8 @@ from benchopt import BaseSolver, safe_import_context
 # - skipping import to speed up autocompletion in CLI.
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
-    from benchopt.stopping_criterion import SingleRunCriterion
     from sklearn.dummy import DummyClassifier
+    from benchmark_utils import channels_dropout, smooth_timemask
 
 
 # The benchmark solvers must be named `Solver` and
@@ -13,10 +13,14 @@ with safe_import_context() as import_ctx:
 
 
 class Solver(BaseSolver):
-
-    name = 'Dummyclassifier'
-
-    stopping_criterion = SingleRunCriterion()
+    name = "Dummyclassifier"
+    parameters = {
+        "augmentation": [
+            ("SmoothTimeMask"),
+            ("ChannelsDropout"),
+            ("IdentityTransform"),
+        ]
+    }
 
     def set_objective(self, X, y, sfreq):
         # Define the information received by each solver from the objective.
@@ -29,7 +33,18 @@ class Solver(BaseSolver):
 
     def run(self, n_iter):
         # This is the function that is called to evaluate the solver.
-        self.clf.fit(self.X, self.y)
+        if self.augmentation == "ChannelsDropout":
+            X, y = channels_dropout(self.X, self.y, n_augmentation=n_iter)
+
+        elif self.augmentation == "SmoothTimeMask":
+            X, y = smooth_timemask(
+                self.X, self.y, n_augmentation=n_iter, sfreq=self.sfreq
+            )
+
+        self.clf.fit(X, y)
+
+    def get_next(self, n_iter):
+        return n_iter + 1
 
     def get_result(self):
         # Return the result from one optimization run.
