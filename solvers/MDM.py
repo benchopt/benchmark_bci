44776e82f71c8624018c9892fa8640182ba76d56
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-07-04 17:18:33 +0200

    Cleaning and changing logic

--- solvers/MDM.py
+++ solvers/MDM.py
@@ -8,7 +8,6 @@ with safe_import_context() as import_ctx:
     from sklearn.pipeline import make_pipeline
     from pyriemann.estimation import Covariances
     from pyriemann.classification import MDM
-    from benchopt.stopping_criterion import SingleRunCriterion
     from benchmark_utils import transformX_moabb
     from benchmark_utils import channels_dropout, smooth_timemask
 # The benchmark solvers must be named `Solver` and
@@ -18,21 +17,15 @@ with safe_import_context() as import_ctx:
 class Solver(BaseSolver):
     name = "MDM"
     parameters = {
-        "augmentation, n_augmentation": [
-            ("SmoothTimeMask", 2),
-            ("ChannelsDropout", 2),
-            ("ChannelsDropout", 3),
-            ("ChannelsDropout", 5),
-            ("SmoothTimeMask", 3),
-            ("SmoothTimeMask", 5),
-            ("IdentityTransform", None),
+        "augmentation": [
+            ("SmoothTimeMask"),
+            ("ChannelsDropout"),
+            ("IdentityTransform"),
         ]
     }
 
     install_cmd = "conda"
     requirements = ["pyriemann", "pip:torch", "pip:braindecode"]
-
-    stopping_criterion = SingleRunCriterion()
 
     def set_objective(self, X, y, sfreq):
         # Define the information received by each solver from the objective.
@@ -40,25 +33,26 @@ class Solver(BaseSolver):
         # `Objective.get_objective`. This defines the benchmark's API for
         # passing the objective to the solver.
         # It is customizable for each benchmark.
-        if self.augmentation == "ChannelsDropout":
-            X, y = channels_dropout(X, y, n_augmentation=self.n_augmentation)
-
-        elif self.augmentation == "SmoothTimeMask":
-            X, y = smooth_timemask(
-                X, y, n_augmentation=self.n_augmentation, sfreq=sfreq
-            )
-
-        else:
-            X = transformX_moabb(X)
-
-        self.X, self.y = X, y
+        X = transformX_moabb(X)
 
         self.X, self.y = X, y
         self.clf = make_pipeline(Covariances("oas"), MDM(metric="riemann"))
+        self.sfreq = sfreq
 
     def run(self, n_iter):
         # This is the function that is called to evaluate the solver.
-        self.clf.fit(self.X, self.y)
+        if self.augmentation == "ChannelsDropout":
+            X, y = channels_dropout(self.X, self.y, n_augmentation=n_iter)
+
+        elif self.augmentation == "SmoothTimeMask":
+            X, y = smooth_timemask(
+                self.X, self.y, n_augmentation=n_iter, sfreq=self.sfreq
+            )
+
+        self.clf.fit(X, y)
+
+    def get_next(self, n_iter):
+        return n_iter + 1
 
     def get_result(self):
         # Return the result from one optimization run.
