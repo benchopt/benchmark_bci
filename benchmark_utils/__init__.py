ef09b820aafa9c80156f590d0e8cabdc34751b80
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-07-04 17:09:22 +0200

    Adding docs

--- benchmark_utils/__init__.py
+++ benchmark_utils/__init__.py
@@ -23,11 +23,21 @@ with safe_import_context() as import_ctx:
 
 def list_train_test(test, list):
     list_test = [list[test]]
-    list_train = list[:test] + list[test+1:]
+    list_train = list[:test] + list[test + 1 :]
     return list_test, list_train
 
 
 def transformX_moabb(X):
+    """Transform X from moabb to numpy array.
+    Parameters:
+    -----------
+    X: ndarray
+        Data to transform.
+    Returns:
+    --------
+    X0: ndarray
+        Transformed data.
+    """
     X0 = []
     for i in range(len(X)):
         X0.append(X[i])
@@ -36,22 +46,34 @@ def transformX_moabb(X):
 
 
 def windows_data(dataset, paradigm_name):
+    """Create windows from the dataset.
 
-    # défintion du paradigm
-    if paradigm_name == 'LeftRightImagery':
-        mapping = {'left_hand': 1, 'right_hand': 2}
+    Parameters:
+    -----------
+    dataset: MOABBDataset
+        Dataset to use.
+    paradigm_name: str
+        Name of the paradigm to use.
+    Returns:
+    --------
+    windows_dataset: WindowsDataset
+        Dataset with windows.
+    sfreq: float
+        Sampling frequency of the dataset.
+    """
 
-    elif paradigm_name == 'MotorImagery':
-        mapping = {'left_hand': 1,
-                   'right_hand': 2,
-                   'feet': 4,
-                   'tongue': 3}
+    # Define mapping of classes to integers
+    # We use two classes from the dataset
+    # 1. left-hand vs right-hand motor imagery
+    if paradigm_name == "LeftRightImagery":
+        mapping = {"left_hand": 1, "right_hand": 2}
+
+    elif paradigm_name == "MotorImagery":
+        mapping = {"left_hand": 1, "right_hand": 2, "feet": 4, "tongue": 3}
 
     low_cut_hz = 4.0  # low cut frequency for filtering
     high_cut_hz = 38.0  # high cut frequency for filtering
     # Parameters for exponential moving standardization
-    factor_new = 1e-3
-    init_block_size = 1000
 
     preprocessors = [
         Preprocessor("pick_types", eeg=True, meg=False, stim=False),
@@ -61,18 +83,12 @@ def windows_data(dataset, paradigm_name):
             # Convert from V to uV
             factor=1e6,
         ),
-        Preprocessor("filter", l_freq=low_cut_hz, h_freq=high_cut_hz),
         # Bandpass filter
-        Preprocessor(
-            exponential_moving_standardize,
-            # Exponential moving standardization
-            factor_new=factor_new,
-            init_block_size=init_block_size,
-        ),
+        Preprocessor("filter", l_freq=low_cut_hz, h_freq=high_cut_hz),
     ]
 
     # Transform the data
-    preprocess(dataset, preprocessors, verbose=False)
+    preprocess(dataset, preprocessors, n_jobs=-1)
     trial_start_offset_seconds = -0.5
     # Extract sampling frequency, check that they are same in all datasets
     sfreq = dataset.datasets[0].raw.info["sfreq"]
@@ -88,26 +104,22 @@ def windows_data(dataset, paradigm_name):
         dataset,
         trial_start_offset_samples=trial_start_offset_samples,
         trial_stop_offset_samples=0,
-        preload=True, mapping=mapping
+        preload=True,
+        mapping=mapping,
     )
 
-    # if we don't want to process the data we can return
-    # windows_dataset_bis =
-    # create_windows_from_events(dataset, mapping=mapping)
     return (windows_dataset, sfreq)
 
 
 def channels_dropout(X, y, n_augmentation):
     seed = 20200220
-    transform = ChannelsDropout(probability=0.5,
-                                random_state=seed)
+    transform = ChannelsDropout(probability=0.5, random_state=seed)
     X_augm = transformX_moabb(X)
     y_augm = y
     for i in range(n_augmentation):
-
-        X_tr, _ = transform.operation(torch.as_tensor(X).float(),
-                                      None,
-                                      p_drop=0.2)
+        X_tr, _ = transform.operation(
+            torch.as_tensor(X).float(), None, p_drop=0.2
+        )
 
         X_tr = X_tr.numpy()
         X_augm = np.concatenate((X_augm, X_tr))
@@ -120,28 +132,25 @@ def smooth_timemask(X, y, n_augmentation, sfreq):
     second = 0.1
     seed = 20200220
 
-    transform = SmoothTimeMask(probability=0.5,
-                               mask_len_samples=int(sfreq * second),
-                               random_state=seed)
+    transform = SmoothTimeMask(
+        probability=0.5,
+        mask_len_samples=int(sfreq * second),
+        random_state=seed,
+    )
 
     X_torch = torch.as_tensor(X).float()
     y_torch = torch.as_tensor(y).float()
-    param_augm = transform.get_augmentation_params(X_torch,
-                                                   y_torch)
-    mls = param_augm['mask_len_samples']
-    msps = param_augm['mask_start_per_sample']
+    param_augm = transform.get_augmentation_params(X_torch, y_torch)
+    mls = param_augm["mask_len_samples"]
+    msps = param_augm["mask_start_per_sample"]
 
     X_augm = transformX_moabb(X)
     y_augm = y
 
     for i in range(n_augmentation):
-
         X_tr, _ = transform.operation(
-                  X_torch,
-                  None,
-                  mask_len_samples=mls,
-                  mask_start_per_sample=msps
-                        )
+            X_torch, None, mask_len_samples=mls, mask_start_per_sample=msps
+        )
 
         X_tr = X_tr.numpy()
         X_augm = np.concatenate((X_augm, X_tr))
