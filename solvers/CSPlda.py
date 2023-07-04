32a5991b3aec6163fd1dd17be7b864da48cd63e4
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-07-04 17:15:52 +0200

    Cleaning and changing the logic for the data augmentantion

--- solvers/CSPlda.py
+++ solvers/CSPlda.py
@@ -7,8 +7,6 @@ with safe_import_context() as import_ctx:
     from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
     from mne.decoding import CSP
     from sklearn.pipeline import make_pipeline
-    from benchopt.stopping_criterion import SingleRunCriterion
-    from benchmark_utils import transformX_moabb
     from benchmark_utils import channels_dropout, smooth_timemask
 
 # The benchmark solvers must be named `Solver` and
@@ -19,17 +17,11 @@ class Solver(BaseSolver):
     name = "CSPLDA"
     parameters = {
         "augmentation, n_augmentation": [
-            ("SmoothTimeMask", 2),
-            ("ChannelsDropout", 2),
-            ("ChannelsDropout", 3),
-            ("ChannelsDropout", 5),
-            ("SmoothTimeMask", 3),
-            ("SmoothTimeMask", 5),
-            ("IdentityTransform", None),
+            ("SmoothTimeMask"),
+            ("ChannelsDropout"),
+            ("IdentityTransform"),
         ]
     }
-
-    stopping_criterion = SingleRunCriterion()
 
     install_cmd = "conda"
     requirements = ["mne", "pip:torch", "pip:braindecode"]
@@ -41,28 +33,28 @@ class Solver(BaseSolver):
         # passing the objective to the solver.
         # It is customizable for each benchmark.
 
-        if self.augmentation == "ChannelsDropout":
-            X, y = channels_dropout(X, y, n_augmentation=self.n_augmentation)
-
-        elif self.augmentation == "SmoothTimeMask":
-            X, y = smooth_timemask(
-                X, y, n_augmentation=self.n_augmentation, sfreq=sfreq
-            )
-
-        else:
-            X = transformX_moabb(X)
-
         self.X, self.y = X, y
         self.clf = make_pipeline(CSP(n_components=8), LDA())
 
     def run(self, n_iter):
         # This is the function that is called to evaluate the solver.
-        self.clf.fit(self.X, self.y)
+        if self.augmentation == "ChannelsDropout":
+            X, y = channels_dropout(self.X, self.y, n_augmentation=n_iter)
+
+        elif self.augmentation == "SmoothTimeMask":
+            X, y = smooth_timemask(
+                self.X, self.y, n_augmentation=n_iter, sfreq=self.sfreq
+            )
+
+        self.clf.fit(X, y)
+
+    def get_next(self, n_iter):
+        return n_iter + 1
 
     def get_result(self):
         # Return the result from one optimization run.
         # The outputs of this function are the arguments of `Objective.compute`
-        # This defines the benchmark's APIafor solvers' results.
+        # This defines the benchmark's API for solvers' results.
         # it is customizable for each benchmark.
         return self.clf
 
