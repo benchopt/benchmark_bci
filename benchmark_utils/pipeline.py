492e82ac2bfab2420d67d5868d2c512e0771380b
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-07-05 17:59:15 +0200

    Updating the pipeline folder

--- benchmark_utils/pipeline.py
+++ benchmark_utils/pipeline.py
@@ -7,7 +7,7 @@ with safe_import_context() as import_ctx:
     from moabb.pipelines.utils import parse_pipelines_from_directory
 
 
-def parser_pipelines(dir_path="/home/bru/PycharmProjects/moabb/pipelines",
+def parser_pipelines(dir_path="pipelines",
                      filtering_paradigm=None):
     """
     Read pipelines from a directory and return a list of pipelines.
