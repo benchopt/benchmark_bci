76c8885de3250d89b535dc90459460ea0e414b48
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-07-06 15:01:33 +0200

    Simplifying stuffs

--- objective.py
+++ objective.py
@@ -4,12 +4,14 @@ from benchopt import BaseObjective, safe_import_context
 # - skipping import to speed up autocompletion in CLI.
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
+    from numpy import array
+
     from sklearn.dummy import DummyClassifier
     from sklearn.model_selection import train_test_split
     from sklearn.metrics import balanced_accuracy_score as BAS
-    import numpy as np
-    from skorch.helper import SliceDataset, to_numpy
 
+    from skorch.helper import SliceDataset, to_numpy
+    from benchmark_utils.dataset import split_windows_train_test
 # The benchmark objective must be named `Objective` and
 # inherit from `BaseObjective` for `benchopt` to work properly.
 
@@ -25,13 +27,13 @@ class Objective(BaseObjective):
 
     link = 'pip: git+https://github.com/Neurotechx/moabb@develop#egg=moabb'
     intall_cmd = 'conda'
-    requirements = [str(link),
+    requirements = [link,
                     'scikit-learn']
 
     parameters = {
         'evaluation_process, subject, subject_test, session_test': [
             ('intra_subject', 1, None, None),
-             ],
+        ],
     }
     # The solvers will train on all the subject except subject_test.
     # It will be the same for the sessions.
@@ -39,12 +41,10 @@ class Objective(BaseObjective):
     # Bump it up if the benchmark depends on a new feature of benchopt.
     min_benchopt_version = "1.3.2"
 
-    def set_data(self, dataset, paradigm_name, sfreq):
+    def set_data(self, dataset, paradigm_name):
         # The keyword arguments of this function are the keys of the dictionary
         # returned by `Dataset.get_data`. This defines the benchmark's
         # API to pass data. This is customizable for each benchmark.
-        # The dictionary defines the keyword arguments
-        # for `Objective.set_data`
 
         data_split_subject = dataset.split('subject')
 
@@ -52,7 +52,7 @@ class Objective(BaseObjective):
 
             dataset = data_split_subject[str(self.subject)]
             X = SliceDataset(dataset, idx=0)
-            y = np.array([y for y in SliceDataset(dataset, idx=1)])
+            y = array(list(SliceDataset(dataset, idx=1)))
 
             # maybe we need to do here different process for each subjects
 
@@ -70,16 +70,13 @@ class Objective(BaseObjective):
                 if i != sujet_test:
                     data_subject_train += data_split_subject[str(i)]
 
-            X_test = SliceDataset(data_subject_test, idx=0)
-            y_test = np.array([y for y in SliceDataset(data_subject_test,
-                                                       idx=1)])
+            splitted_data = split_windows_train_test(data_subject_train,
+                                                     data_subject_test)
 
-            X_train = SliceDataset(data_subject_train, idx=0)
-            y_train = np.array([y for y in SliceDataset(data_subject_train,
-                                                        idx=1)])
-
-            self.X_train, self.y_train = X_train,  y_train
-            self.X_test, self.y_test = X_test, y_test
+            self.X_train = splitted_data['X_train']
+            self.y_train = splitted_data['y_train']
+            self.X_test = splitted_data['X_test']
+            self.y_test = splitted_data['y_test']
 
         elif self.evaluation_process == 'inter_session':
 
@@ -91,22 +88,18 @@ class Objective(BaseObjective):
             for key in data_split_session.items():
                 if key[0] != str(session_test):
                     data_session_train += data_split_session[key[0]]
-            X_test = SliceDataset(data_session_test, idx=0)
-            y_test = np.array([y for y in SliceDataset(data_session_test,
-                                                       idx=1)])
-            X_train = SliceDataset(data_session_train, idx=0)
-            y_train = np.array([y for y in SliceDataset(data_session_train,
-                                                        idx=1)])
 
-            self.X_train, self.y_train = X_train, y_train
-            self.X_test, self.y_test = X_test, y_test
+            splitted_data = split_windows_train_test(data_session_train,
+                                                     data_session_test)
 
-        self.sfreq = sfreq
+            self.X_train = splitted_data['X_train']
+            self.y_train = splitted_data['y_train']
+            self.X_test = splitted_data['X_test']
+            self.y_test = splitted_data['y_test']
 
         return dict(
-                X_train=X_train, y_train=y_train,
-                X_test=X_test, y_test=y_test,
-                sfreq=sfreq
+            X_train=X_train, y_train=y_train,
+            X_test=X_test, y_test=y_test,
         )
 
     def compute(self, model):
@@ -139,10 +132,6 @@ class Objective(BaseObjective):
         # for `Solver.set_objective`. This defines the
         # benchmark's API for passing the objective to the solver.
         # It is customizable for each benchmark.
-
-        # note for de cross validation :
-        # faire un split ici en fonction des différentes seed
-        # et faire la commande -r de benchopt pour répeter
 
         return dict(
             X=self.X_train,
