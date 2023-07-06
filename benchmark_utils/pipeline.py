a44952822debc35b40fed68510f6b463e051446b
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-07-06 15:00:59 +0200

    Separating module and cleaning functions

--- benchmark_utils/pipeline.py
+++ benchmark_utils/pipeline.py
@@ -7,18 +7,17 @@ with safe_import_context() as import_ctx:
     from moabb.pipelines.utils import parse_pipelines_from_directory
 
 
-def parser_pipelines(dir_path="pipelines",
-                     filtering_paradigm=None):
+def parser_pipelines(dir_path="pipelines", filtering_paradigm=None):
     """
     Read pipelines from a directory and return a list of pipelines.
     """
     if filtering_paradigm is None:
-        filtering_paradigm = ['LeftRightImagery', 'MotorImagery']
+        filtering_paradigm = ["LeftRightImagery", "MotorImagery"]
 
     pipeline_configs = parse_pipelines_from_directory(dir_path)
     pipelines = {}
     for pipeline in pipeline_configs:
 
-        if any(par in filtering_paradigm for par in pipeline['paradigms']):
-            pipelines.update({pipeline['name']: pipeline['pipeline']})
+        if any(par in filtering_paradigm for par in pipeline["paradigms"]):
+            pipelines.update({pipeline["name"]: pipeline["pipeline"]})
     return pipelines
