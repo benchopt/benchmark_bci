098f1752b4feab6ccc01be8a46d1ed81331d35da
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-07-06 15:01:51 +0200

    Implementing the AugmentedBCISolver

--- solvers/MDM.py
+++ solvers/MDM.py
@@ -1,4 +1,4 @@
-from benchopt import BaseSolver, safe_import_context
+from benchopt import safe_import_context
 
 
 # Protect the import with `safe_import_context()`. This allows:
@@ -10,24 +10,31 @@ with safe_import_context() as import_ctx:
     from pyriemann.classification import MDM
     from benchmark_utils.transformation import (
         channels_dropout,
-        smooth_timemask, Covariances_augm
+        smooth_timemask,
     )
+    from benchmark_utils.augmented_dataset import (
+        AugmentedBCISolver,
+    )
+    from benchmark_utils.augmented_method import (
+        Covariances_augm
+    )
+
     from skorch.helper import to_numpy
 # The benchmark solvers must be named `Solver` and
 # inherit from `BaseSolver` for `benchopt` to work properly.
 
 
-class Solver(BaseSolver):
+class Solver(AugmentedBCISolver):
     name = "MDM"
     parameters = {
         "augmentation": [
-            ("SmoothTimeMask"),
-            ("Barycenter")
+            "SmoothTimeMask",
+            "Barycenter"
         ]
     }
 
     install_cmd = "conda"
-    requirements = ["pyriemann", "pip:torch", "pip:braindecode"]
+    requirements = ["pyriemann"]
 
     def set_objective(self, X, y, sfreq):
         # Define the information received by each solver from the objective.
@@ -35,13 +42,18 @@ class Solver(BaseSolver):
         # `Objective.get_objective`. This defines the benchmark's API for
         # passing the objective to the solver.
         # It is customizable for each benchmark.
-        X = to_numpy(X)
 
-        self.X, self.y = X, y
+        self.sfreq = X.datasets[0].raw.info["sfreq"]
+        self.X = to_numpy(X)
+        self.y = y
         self.clf = make_pipeline(Covariances("oas"), MDM(metric="riemann"))
-        self.sfreq = sfreq
 
     def run(self, n_iter):
+        """
+        Here, we override the `run` method of the `AugmentedBCISolver` class.
+        to adding the Barycenter augmentation method implemented by Cristopher.
+        """
+
         # This is the function that is called to evaluate the solver.
         if self.augmentation == "ChannelsDropout":
             X, y = channels_dropout(self.X, self.y, n_augmentation=n_iter)
@@ -61,16 +73,3 @@ class Solver(BaseSolver):
             y = self.y
 
         self.clf.fit(X, y)
-
-    def get_next(self, n_iter):
-        return n_iter + 1
-
-    def get_result(self):
-        # Return the result from one optimization run.
-        # The outputs of this function are the arguments of `Objective.compute`
-        # This defines the benchmark's API for solvers' results.
-        # it is customizable for each benchmark.
-        return self.clf
-
-    def warmup_solver(self):
-        pass
