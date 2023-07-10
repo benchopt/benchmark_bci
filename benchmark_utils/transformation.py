826b81f4a5c5b3b10190651bf2b9b21ec8f4d8c5
chris-mrn <109912652+chris-mrn@users.noreply.github.com>
2023-07-10 12:02:30 +0200

    changing the place of transform to generate different augmentation at each iteration

--- benchmark_utils/transformation.py
+++ benchmark_utils/transformation.py
@@ -6,7 +6,7 @@ from benchopt import safe_import_context
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     import numpy as np
-
+    import matplotlib.pyplot as plt
     from numpy import concatenate
     from torch import as_tensor
     from skorch.helper import to_numpy
@@ -14,7 +14,7 @@ with safe_import_context() as import_ctx:
 
 
 def channels_dropout(
-    X, y, n_augmentation, seed=0, probability=0.5, p_drop=0.2
+    X, y, n_augmentation, probability=0.5, p_drop=0.2
 ):
     """
     Function to apply channels dropout to X raw data
@@ -43,10 +43,11 @@ def channels_dropout(
         The labels.
 
     """
-    transform = ChannelsDropout(probability=probability, random_state=seed)
+
     X_augm = to_numpy(X)
     y_augm = y
     for i in range(n_augmentation):
+        transform = ChannelsDropout(probability=probability)
         X_tr, _ = transform.operation(
             as_tensor(X).float(), None, p_drop=p_drop
         )
@@ -59,34 +60,36 @@ def channels_dropout(
 
 
 def smooth_timemask(
-    X, y, n_augmentation, sfreq, seed=0, probability=0.5, second=0.1
+    X, y, n_augmentation, sfreq, probability=0.8, second=0.2
 ):
     """
     Function to apply smooth time mask to X raw data
     and concatenate it to the original data.
     """
 
-    transform = SmoothTimeMask(
-        probability=probability,
-        mask_len_samples=int(sfreq * second),
-        random_state=seed,
-    )
-
     X_torch = as_tensor(np.array(X)).float()
     y_torch = as_tensor(y).float()
-    param_augm = transform.get_augmentation_params(X_torch, y_torch)
-    mls = param_augm["mask_len_samples"]
-    msps = param_augm["mask_start_per_sample"]
-
     X_augm = to_numpy(X)
     y_augm = y
 
+    mask_len_samples = int(sfreq * second)
     for i in range(n_augmentation):
+
+        transform = SmoothTimeMask(
+                                probability=probability,
+                                mask_len_samples=mask_len_samples,
+                                    )
+
+        param_augm = transform.get_augmentation_params(X_torch, y_torch)
+        mls = param_augm["mask_len_samples"]
+        msps = param_augm["mask_start_per_sample"]
+
         X_tr, _ = transform.operation(
             X_torch, None, mask_len_samples=mls, mask_start_per_sample=msps
         )
-
         X_tr = X_tr.numpy()
+        plt.plot(X_tr[0][0])
+        plt.show()
         X_augm = concatenate((X_augm, X_tr))
         y_augm = concatenate((y_augm, y))
 
