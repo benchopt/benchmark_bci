cfa659d8412c58252f3d2d78491272a8642a5897
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-05-04 17:48:19 +0200

    reverting the y fixing

--- solvers/ShallowFBCSPNet.py
+++ solvers/ShallowFBCSPNet.py
@@ -135,12 +135,7 @@ class Solver(BaseSolver):
         self.clf = clf
 
         self.X = X
-        have_0_label = any([label == 0 for label in y])
-
-        if have_0_label:
-            self.y = array([label for label in y])
-        else:
-            self.y = array([label-1 for label in y])
+        self.y = y
 
     def run(self, n_iter):
         # This is the function that is called to evaluate the solver
