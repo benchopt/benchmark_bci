5f0f23d4ca919d541750c0f30d3da6d197d2402d
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-06-03 23:27:19 +0200

    fix the bnci dataset

--- datasets/BNCI.py
+++ datasets/BNCI.py
@@ -34,5 +34,4 @@ class Dataset(BaseDataset):
 
         dataset, sfreq = windows_data(data, self.paradigm_name)
 
-        return dict(dataset=dataset, sfreq=sfreq,
-                    paradigm_name=self.paradigm_name)
+        return dict(dataset=dataset, sfreq=sfreq)
