00cb88c6c46e062287a265555cb7150f826c2230
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-06-04 17:42:45 +0200

    creating a function that gererate the parameters based on the pipeline step

--- benchmark_utils/pipeline.py
+++ benchmark_utils/pipeline.py
@@ -23,88 +23,217 @@ def parser_pipelines(dir_path="pipelines", filtering_paradigm=None):
     return pipelines
 
 
-def get_hyperparams_from_pipeline(pipeline, trial):
+def get_parameters_for_layer(name, trial):
     """
-    Get the parameters from a pipeline.
+    Get the hyperparameters for a pipeline-layer.
+
+    Parameters
+    ----------
+    name: str
+        Name of the pipeline-layer.
+    trial: optuna.Trial
+        Optuna trial object.
+
+    Returns
+    -------
+    dict
+        Dictionary with the hyperparameters for the pipeline-layer.
     """
-    if pipeline == "DUMMY":
-        strategy_options = ["most_frequent", "prior", "stratified", "uniform"]
-        strategy = trial.suggest_categorical("strategy", strategy_options)
-        param = dict(dummyclassifer=dict(strategy=strategy))
-    elif pipeline == "MDM":
-        # This method is COV -> MDM
-        # Covariance matrix parameters
-        estimator = trial.suggest_categorical("estimator", ["cov", "lwf"])
-        # MDM parameters
-        metric_options = ["riemann", "logeuclid"]
-        metric = trial.suggest_categorical("metric", metric_options)
-        param = dict(
-            mdm=dict(metric=metric), covariances=dict(estimator=estimator)
-        )
-    elif pipeline == "TRCSPLDA":
-        estimator = trial.suggest_categorical("estimator", ["cov", "lwf"])
-        n_filters = trial.suggest_int("n_filters", 6, 10)
-        shrinkage = trial.suggest_float("shrinkage", 0, 1)
-        solver = trial.suggest_categorical("solver", ["eigen"])
-
-        param = dict(
-            lineardiscriminantanalysis=dict(
-                shrinkage=shrinkage, solver=solver
-            ),
-            covariances=dict(estimator=estimator),
-            trcsp=dict(nfilter=n_filters),
-        )
-
-    elif pipeline == "TangentSpaceSVMGrid":
-        # This method is:
-        # COV -> TANGENTSPACE -> SVC
-        # Parameters for the covariance matrix
-        cov_estimator = ["corr", "cov", "hub", "lwf"]
-
-        cov_estimator = trial.suggest_categorical(
-            "cov_estimator", cov_estimator
-        )
-
+    name = name.upper()
+    if name == "SVM":
         # Parameters for the SVC
         svm_C = trial.suggest_float("svm_C", 1e-6, 1e6, log=True)
         svm_kernel = trial.suggest_categorical("svm_kernel", ["linear", "rbf"])
-
-        param = dict(
-            covariances=dict(estimator=cov_estimator),
-            svc=dict(C=svm_C, kernel=svm_kernel),
+        svc = dict(C=svm_C, kernel=svm_kernel)
+        return svc
+    if name == "COV":
+        # Parameters for the covariance matrix
+        cov_estimator = trial.suggest_categorical(
+            "cov_estimator", ["cov", "hub", "lwf", "oas"]
         )
-    elif pipeline == "AUGTangSVMGrid":
-        # This method is:
-        # AUGMENTATION -> COV -> TANGENTSPACE -> SVC
-        # Parameters for the augmentation
+        cov = dict(estimator=cov_estimator)
+        return cov
+    if name == "LDA":
+        # Parameters for the LDA
+        shrinkage = trial.suggest_float("shrinkage", 0, 1)
+        solver = trial.suggest_categorical("solver", ["lsqr", "eigen"])
+        lda = dict(shrinkage=shrinkage, solver=solver)
+        return lda
+    if name == "MDM":
+        metric_options = ["riemann", "logeuclid", "euclid"]
+        metric = trial.suggest_categorical("metric", metric_options)
+        mdm = dict(metric=metric)
+        return mdm
+    if name == "TRCSP":
+        n_filters = trial.suggest_int("n_filters", 6, 10)
+        trcsp = dict(nfilter=n_filters)
+        return trcsp
+    if name == "AUG":
         lag = trial.suggest_int("lag", 1, 10)
         order = trial.suggest_int("order", 1, 10)
-
-        # Parameters for the SVC
-        svm_C = trial.suggest_float("svm_C", 1e-10, 1e10, log=True)
-        svm_kernel = trial.suggest_categorical("svm_kernel", ["linear", "rbf"])
-
-        param = dict(
-            augmenteddataset=dict(order=order, lag=lag),
-            svc=dict(C=svm_C, kernel=svm_kernel),
-        )
-    elif pipeline == "COVCSPLDA":
-        # This method is:
-        # COV -> CSP -> LDA
-        # Parameters for the covariance matrix
-        cov_estimator = ["cov", "hub", "lwf"]
-        cov_estimator = trial.suggest_categorical(
-            "cov_estimator", cov_estimator
-        )
-
+        aug = dict(lag=lag, order=order)
+        return aug
+    if name == "CSP":
         # Parameters for the CSP
         nfilter = trial.suggest_int("nfilter", 6, 10)
         metric = trial.suggest_categorical("metric", ["euclid"])
         log = trial.suggest_categorical("log", [True, False])
+        csp = dict(nfilter=nfilter, metric=metric, log=log)
+        return csp
+    if name == "FGMDM":
+        # I am not sure about this!!!
+
+        metric_options = ["riemann", "logeuclid", "euclid"]
+        metric = trial.suggest_categorical("metric", metric_options)
+        fgmdm = dict(metric=metric)
+        return fgmdm
+    if name == "LOGREG":
+        # Optuna parameters for the Logistic Regression
+        penalty = trial.suggest_categorical("penalty", ["l1", "l2"])
+        C = trial.suggest_float("C", 1e-6, 1e6, log=True)
+        solver = trial.suggest_categorical("solver", ["lbfgs", "liblinear"])
+        max_iter = trial.suggest_int("max_iter", 100, 1000)
+        logreg = dict(penalty=penalty, C=C, solver=solver, max_iter=max_iter)
+        return logreg
+    if name == "LogReg_ElNet".upper():
+        l1_ratio = trial.suggest_float("l1_ratio", 0.2, 0.75)
+        logreg = dict(l1_ratio=l1_ratio)
+        return logreg
+
+
+def get_hyperparams_from_pipeline(pipeline, trial):
+    """
+    Get the parameters from a pipeline.
+    """
+    option_valid = [
+        "Aug-Cov-Tang-SVM",  # ok
+        "Cov-CSP-LDA_shr",  # talk with Thomas
+        "Cov-CSP-LDA_svd",  # talk with Thomas
+        "Cov-FgMDM",  # need to check
+        "Cov-MDM",  # okay
+        "Cov-Tang-LogReg",  # okay
+        "Cov-Tang-LogReg_ElNet",  # okay
+        "Cov-Tang-SVM",
+        "Cov-TRCSP-LDA",
+        "DUMMY",
+        "LogVar-LDA",
+        "LogVar-SVM",
+    ]
+
+    if pipeline == "Aug-Cov-Tang-SVM":
+        # Parameters for the augmentation
+        aug = get_parameters_for_layer("AUG", trial)
+        # Parameters for the SVC
+        svm = get_parameters_for_layer("SVM", trial)
 
         param = dict(
-            covariances=dict(estimator=cov_estimator),
-            csp=dict(nfilter=nfilter, metric=metric, log=log),
-            lineardiscriminantanalysis=dict(solver="lsqr", shrinkage="auto"),
+            augmenteddataset=aug,
+            svc=svm,
         )
+    # Talk with Thomas about these pipelineS
+    elif pipeline == "Cov-CSP-LDA_shr" or pipeline == "Cov-CSP-LDA_svd":
+        # This method is:
+        # COV -> CSP -> LDA
+        # Parameters for the covariance matrix
+        covariances = get_parameters_for_layer("COV", trial)
+        # Parameters for the CSP
+        csp = get_parameters_for_layer("CSP", trial)
+        # Parameters for the LDA
+        lda = get_parameters_for_layer("LDA", trial)
+
+        param = dict(
+            covariances=covariances,
+            csp=csp,
+            lineardiscriminantanalysis=lda,
+        )
+    elif pipeline == "Cov-FgMDM":
+        # This method is:
+        # COV -> FgMDM
+        # Parameters for the covariance matrix
+        covariances = get_parameters_for_layer("COV", trial)
+        # Parameters for the Fg MDM
+        fgmdm = get_parameters_for_layer("FgMDM", trial)
+        # Check if this correct
+        param = dict(
+            covariances=covariances,
+            fgmdm=fgmdm,
+        )
+
+    elif pipeline == "Cov-MDM":
+        # Covariance matrix parameters
+        covariance = get_parameters_for_layer("COV", trial)
+        # MDM parameters
+        mdm = get_parameters_for_layer("MDM", trial)
+
+        param = dict(mdm=mdm, covariances=covariance)
+    elif pipeline == "Cov-Tang-LogReg":
+        # This method is:
+        # COV -> TANGENTSPACE -> LOGREG
+        # Parameters for the covariance matrix
+        covariances = get_parameters_for_layer("COV", trial)
+        # Parameters for the Logistic Regression
+        logreg = get_parameters_for_layer("LogReg", trial)
+
+        param = dict(
+            covariances=covariances,
+            tangentspace=dict(metric="riemann"),
+            logisticregression=logreg,
+        )
+    elif pipeline == "Cov-Tang-LogReg_ElNet":
+        # This method is:
+        # COV -> TANGENTSPACE -> LOGREG
+        # Parameters for the covariance matrix
+        covariances = get_parameters_for_layer("COV", trial)
+        # Parameters for the Logistic Regression
+        logreg = get_parameters_for_layer("LogReg_ElNet", trial)
+
+        param = dict(
+            covariances=covariances,
+            logisticregression=logreg,
+        )
+    elif pipeline == "Cov-TRCSP-LDA":
+        covariances = get_parameters_for_layer("COV", trial)
+        lda = get_parameters_for_layer("LDA", trial)
+        trcsp = get_parameters_for_layer("TRCSP", trial)
+
+        param = dict(
+            lineardiscriminantanalysis=lda,
+            covariances=covariances,
+            trcsp=trcsp,
+        )
+
+    elif pipeline == "Cov-Tang-SVM":
+        # This method is:
+        # COV -> TANGENTSPACE -> SVC
+        # Parameters for the covariance matrix
+        covariances = get_parameters_for_layer("COV", trial)
+        # Parameters for the SVC
+        svm = get_parameters_for_layer("SVM", trial)
+
+        param = dict(
+            covariances=covariances,
+            svc=svm,
+        )
+    elif pipeline == "LogVar-LDA":
+        # This method is:
+        # LOGVAR -> LDA
+        # Parameters for the LDA
+        lda = get_parameters_for_layer("LDA", trial)
+
+        param = dict(
+            logvar=dict(),
+            lineardiscriminantanalysis=lda,
+        )
+    elif pipeline == "LogVar-SVM":
+        # This method is:
+        # LOGVAR -> SVC
+        # Parameters for the SVC
+        svm = get_parameters_for_layer("SVM", trial)
+
+        param = dict(
+            logvar=dict(),
+            svc=svm,
+        )
+    else:
+        param = dict()
     return param
