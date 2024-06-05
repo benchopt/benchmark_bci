e0893d7eeaa0ff4770c324e5eaba869fafb1e66f
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-06-06 00:16:32 +0200

    reverting objective

--- objective.py
+++ objective.py
@@ -139,5 +139,5 @@ class Objective(BaseObjective):
             X=self.X_train,
             y=self.y_train,
             sfreq=self.sfreq,
-            extra_info=self.extra_info,
-        )
+            extra_info=self.extra_info
+         )
